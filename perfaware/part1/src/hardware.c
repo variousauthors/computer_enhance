ca8af19795b6cf3c73b4749c11e3c2f5ccc65108
various-authors <various.authors@gmail.com>
2025-07-15 14:31:06 -0700

    [WIP] homework - listing 44
    The initial implementation assumed all we would ever do is disassemble,
    the assumption being that when we implement execute we will stop
    disassembling... but that's lame!
    
    So now there needs to be an explicit decode step that stores the
    fields d, w, reg, mod, rm, and the bytes... so that we can then
    pass that information into disassemble... or execute... or both!

--- perfaware/part1/src/hardware.c
+++ perfaware/part1/src/hardware.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*                       AX CX DX BC SP BP SI DI */
 uint16_t REGISTERS[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 // for 8-bit stores
@@ -15,38 +16,42 @@ uint16_t REGISTERS[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 #define STORE_WIDE(index, byte1, byte2)                                        \
   (REGISTERS[index] = (byte2 << 8) | byte1)
 
+// for 16-bit stores
+#define REGISTER_TO_REGISTER_STORE_WIDE(index1, index2)                        \
+  (REGISTERS[index1] = REGISTERS[index2])
+
 void registerStore8(int w, REG_ENCODING reg, uint8_t byte1) {
   switch (w << 3 | reg) {
   case AL: {
-    STORE_LOW(0, byte1);
+    STORE_LOW(AL, byte1);
     break;
   }
   case AH: {
-    STORE_LOW(0, byte1);
+    STORE_HIGH(0b100 | AH, byte1);
     break;
   }
   case BL: {
-    STORE_LOW(1, byte1);
+    STORE_LOW(BL, byte1);
     break;
   }
   case BH: {
-    STORE_LOW(1, byte1);
+    STORE_HIGH(0b100 | BH, byte1);
     break;
   }
   case CL: {
-    STORE_LOW(2, byte1);
+    STORE_LOW(CL, byte1);
     break;
   }
   case CH: {
-    STORE_LOW(2, byte1);
+    STORE_HIGH(0b100 | CH, byte1);
     break;
   }
   case DL: {
-    STORE_LOW(3, byte1);
+    STORE_LOW(DL, byte1);
     break;
   }
   case DH: {
-    STORE_LOW(3, byte1);
+    STORE_HIGH(0b100 | DH, byte1);
     break;
   }
   default:
@@ -59,35 +64,35 @@ void registerStore8(int w, REG_ENCODING reg, uint8_t byte1) {
 void registerStore16(int w, REG_ENCODING reg, uint8_t byte1, uint8_t byte2) {
   switch (w << 3 | reg) {
   case AX: {
-    STORE_WIDE(0, byte1, byte2);
+    STORE_WIDE(reg & 0b0011, byte1, byte2);
     break;
   }
   case BX: {
-    STORE_WIDE(1, byte1, byte2);
+    STORE_WIDE(reg & 0b011, byte1, byte2);
     break;
   }
   case CX: {
-    STORE_WIDE(2, byte1, byte2);
+    STORE_WIDE(reg & 0b011, byte1, byte2);
     break;
   }
   case DX: {
-    STORE_WIDE(3, byte1, byte2);
+    STORE_WIDE(reg & 0b011, byte1, byte2);
     break;
   }
   case SP: {
-    STORE_WIDE(4, byte1, byte2);
+    STORE_WIDE(reg & 0b111, byte1, byte2);
     break;
   }
   case BP: {
-    STORE_WIDE(5, byte1, byte2);
+    STORE_WIDE(reg & 0b111, byte1, byte2);
     break;
   }
   case SI: {
-    STORE_WIDE(6, byte1, byte2);
+    STORE_WIDE(reg & 0b111, byte1, byte2);
     break;
   }
   case DI: {
-    STORE_WIDE(7, byte1, byte2);
+    STORE_WIDE(reg & 0b111, byte1, byte2);
     break;
   }
   default:
@@ -95,4 +100,13 @@ void registerStore16(int w, REG_ENCODING reg, uint8_t byte1, uint8_t byte2) {
     exit(1);
     break;
   }
+}
+
+void registerToRegisterStore16(int w, REG_ENCODING reg1, REG_ENCODING reg2) {
+  REGISTER_TO_REGISTER_STORE_WIDE(reg1, reg2);
+}
+
+void storeRegisterMemoryToFromRegister(int d, int w, int rm, int reg, int byte1,
+                                       int byte2) {
+  int a;
 }
--- perfaware/part1/src/hardware.h
+++ perfaware/part1/src/hardware.h
@@ -6,14 +6,15 @@
 #define BIT_D_MASK 0b00000010
 #define BIT_W_MASK 0b00000001
 #define REG_MASK 0b00111000
+#define RM_MASK 0b00000111
 
 #define IMED_SIGN_EXT_MASK BIT_D_MASK
 #define IMED_CODE_MASK REG_MASK
 
-#define MOV_W_MASK 0b00000001
-#define MOV_D_MASK 0b00000010
-#define MOV_REG_MASK 0b00111000
-#define MOV_RM_MASK 0b00000111
+#define MOV_W_MASK BIT_W_MASK
+#define MOV_D_MASK BIT_D_MASK
+#define MOV_REG_MASK REG_MASK
+#define MOV_RM_MASK RM_MASK
 #define MOV_MOD_MASK 0b11000000
 
 #define MOV_IMM_W_MASK 0b00001000
@@ -29,22 +30,22 @@ enum MOD {
 };
 
 typedef enum REG_ENCODING {
-  AL,
-  CL,
-  DL,
-  BL,
-  AH,
-  CH,
-  DH,
-  BH,
-  AX,
-  CX,
-  DX,
-  BX,
-  SP,
-  BP,
-  SI,
-  DI,
+  AL, // 0000
+  CL, // 0001
+  DL, // 0010
+  BL, // 0011
+  AH, // 0100
+  CH, // 0101
+  DH, // 0110
+  BH, // 0111
+  AX, // 1000
+  CX, // 1001
+  DX, // 1010
+  BX, // 1011
+  SP, // 1100
+  BP, // 1101
+  SI, // 1110
+  DI, // 1111
 } REG_ENCODING;
 
 enum RM_ENCODING {
@@ -71,5 +72,8 @@ enum IMED_CODE {
 
 void registerStore8(int w, REG_ENCODING reg, uint8_t byte1);
 void registerStore16(int w, REG_ENCODING reg, uint8_t byte1, uint8_t byte2);
+
+void storeRegisterMemoryToFromRegister(int d, int w, int rm, int reg, int byte1,
+                                       int byte2);
 
 #endif
